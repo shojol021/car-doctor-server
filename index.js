25558b96ef0000760449a7f7de8b35d41e438212
shojol021 <shojol021@gmail.com>
2023-05-23 08:49:31 +0600

    booking deleteone option created

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
       const result = await bookingCollection.insertOne(booking)
       res.send(result)
     })
+
+    app.delete('/delete/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
